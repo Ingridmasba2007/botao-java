4bfef13cb8ddc97bc63b6758579e5f53d024e8fa
Ingridmasba2007 <127319046+Ingridmasba2007@users.noreply.github.com>
2024-02-27 10:10:19 -0300

    e

--- main.js
+++ main.js
@@ -7,3 +7,7 @@ function mudarCorTexto(){
 function mudarCorFundo (){
     document.querySelector("#p1").style.background="white"
 }
+function alterarTexto(){
+    textoform=document.querySelector("#textinput").value
+    document.querySelector("#texto2").innerHTML=textoForm
+}
